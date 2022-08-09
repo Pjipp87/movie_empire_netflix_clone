aff2f3d3ce3778b501c7dd51f3f4cc9a1e74dfe0
ScarfaceHBC <p.jipp87@gamil.com>
2022-08-09 20:28:39 +0200

    ...

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,9 +1,9 @@
 import logo from "../Assets/images/logo.svg";
-import "../Styles/App.css";
+import "../Styles/WelcomeScreen.css";
 //import axios from "axios";
 
 function WelcomeScreen() {
-  /* 
+  /*
   // TODO: Server Abfrage:
   const [counter, setCounter] = useState(0);
 
@@ -16,20 +16,8 @@ function WelcomeScreen() {
  */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} width="30%"></img>
+      <button>Los</button>
     </div>
   );
 }
