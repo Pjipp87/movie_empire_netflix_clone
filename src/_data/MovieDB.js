45c925e52d653cc7c08055becfbfd5ae47d8f8d8
ScarfaceHBC <p.jipp87@gamil.com>
2022-08-11 19:17:58 +0200

    ....

--- src/_data/MovieDB.js
+++ src/_data/MovieDB.js
@@ -9,8 +9,17 @@ export const MovieDB = [
       "From Marvel Studios and Paramount Pictures comes Iron Man, an action-packed take on the tale of wealthy philanthropist Tony Stark (Robert Downey Jr.), who develops an invulnerable robotic suit to fight the throes of evil. In addition to being filthy rich, billionaire industrialist Tony Stark is also a genius inventor. When Stark is kidnapped and forced to build a diabolical weapon, he instead uses his intelligence and ingenuity to construct an indestructible suit of armor and escape his captors. Once free, Stark discovers a deadly conspiracy that could destabilize the entire globe, and dons his powerful new suit on a mission to stop the villains and save the world. Gwyneth Paltrow co-stars as his secretary, Virginia 'Pepper' Potts, while Terrence Howard fills the role of Jim 'Rhodey' Rhodes, one of Stark's colleagues, whose military background leads him to help in the formation of the suit. Jon Favreau directs, with Marvel movie veterans Avi Arad and Kevin Feige producing. - Jeremy Wheeler, Rovi",
   },
   {
-    name: "Iron Man 2",
+    name: "The Incredible Hulk",
     id: 2,
+    cover: "/src/Assets/images/Covers/The_Incredible_Hulk.jpg",
+    trailer:
+      "http://trailers.apple.com/movies/universal/the_incredible_hulk/the_incredible_hulk-tlr1_h1080p.mov",
+    description:
+      "'The Incredible Hulk' kicks off an all-new, explosive and action-packed epic of one of the most popular Super Heroes of all time. In this new beginning, scientist Bruce Banner (Edward Norton) desperately hunts for a cure to the gamma radiation that poisoned his cells and unleashes the unbridled force of rage within him: The Hulk.Living in the shadows--cut off from a life he knew and the woman he loves, Betty Ross (Liv Tyler)--Banner struggles to avoid the obsessive pursuit of his nemesis, General Thunderbolt Ross (William Hurt), and the military machinery that seeks to capture him and brutally exploit his power. As all three grapple with the secrets that led to The Hulk's creation, they are confronted with a monstrous new adversary known as The Abomination (Tim Roth), whose destructive strength exceeds even The Hulk's own. And on June 13, 2008, one scientist must make an agonizing final choice: accept a peaceful life as Bruce Banner or find heroism in the creature he holds inside",
+  },
+  {
+    name: "Iron Man 2",
+    id: 3,
     cover: "/src/Assets/images/Covers/Iron_Man_2.jpg",
     trailer:
       "http://videos.hd-trailers.net/Iron_Man_2_The_IMAX_Experience_Trailer-1080p.mp4",
