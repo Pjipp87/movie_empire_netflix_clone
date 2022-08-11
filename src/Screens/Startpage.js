a26f6e1f6f5c03c450b1d25a5f5cbf24ed88d91a
ScarfaceHBC <p.jipp87@gamil.com>
2022-08-11 10:58:27 +0200

    ....

--- src/Screens/Startpage.js
+++ src/Screens/Startpage.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import { Player, LoadingSpinner } from "video-react";
-import { useRef, useEffect, useState, useLayoutEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import "../Styles/Startpage.css";
 import {
@@ -8,6 +8,9 @@ import {
   BsPlusLg,
   BsPauseCircle,
   BsArrowsFullscreen,
+  BsVolumeMuteFill,
+  BsVolumeUpFill,
+  BsFullscreenExit,
 } from "react-icons/bs";
 import { VscDebugRestart } from "react-icons/vsc";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
@@ -20,9 +23,12 @@ export const Startpage = () => {
   const [height, setHeight] = useState(0);
   const [show, setShow] = useState(true);
   const [initialShow, setinitialShow] = useState(true);
-  const handle = useFullScreenHandle();
+  const fullscreenHandle = useFullScreenHandle();
+  const [isMuted, setIsMuted] = useState(false);
 
   useEffect(() => {
+    setWidth(startpage.current.offsetWidth);
+    setHeight(playerRef.current.offsetHeight);
     setIsplaying(true);
     playerRef.current.seekTo(7, "seconds");
     const timer = setTimeout(() => {
@@ -30,12 +36,6 @@ export const Startpage = () => {
       setShow(false);
     }, 7000);
     return () => clearTimeout(timer);
-  }, []);
-
-  useLayoutEffect(() => {
-    setWidth(startpage.current.offsetWidth);
-    setHeight(playerRef.current.offsetHeight);
-    console.log("Height: ", height);
   }, []);
 
   const _setShowInfo = () => {
@@ -61,16 +61,69 @@ export const Startpage = () => {
         onMouseEnter={_setShowInfo}
         onMouseLeave={_setHideInfo}
       >
-        <FullScreen handle={handle}>
-          <ReactPlayer
-            ref={playerRef}
-            height={"fit-content"}
-            width={width}
-            className={`trailerPlayer`}
-            playing={isplaying}
-            controls={false}
-            url="https://archive.org/download/turner_video_391170/391170.mp4"
-          />
+        <FullScreen handle={fullscreenHandle}>
+          <div style={{ position: "relative", width: "100%", height: "100%" }}>
+            <ReactPlayer
+              muted={isMuted}
+              ref={playerRef}
+              height={fullscreenHandle.active ? "100%" : "fit-content"}
+              width={fullscreenHandle.active ? "100%" : width}
+              className={`trailerPlayer`}
+              playing={isplaying}
+              controls={false}
+              url="https://archive.org/download/turner_video_391170/391170.mp4"
+            ></ReactPlayer>
+            {fullscreenHandle.active ? (
+              <div
+                //className={`trailerInfo ${show ? "fadeIn" : "fadeOut"}`}
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  padding: "1em 5em",
+                }}
+              >
+                {isplaying ? (
+                  <div
+                    className="infoIconContainer"
+                    onClick={() => setIsplaying(!isplaying)}
+                  >
+                    <BsPauseCircle size={"5em"} />
+                  </div>
+                ) : (
+                  <div
+                    className="infoIconContainer"
+                    onClick={() => setIsplaying(!isplaying)}
+                  >
+                    <BsPlayCircle size={"5em"} />
+                  </div>
+                )}
+                {isMuted ? (
+                  <div className="infoIconContainer">
+                    <BsVolumeMuteFill
+                      size="5em"
+                      onClick={() => setIsMuted(!isMuted)}
+                    />
+                  </div>
+                ) : (
+                  <div className="infoIconContainer">
+                    <BsVolumeUpFill
+                      size="5em"
+                      onClick={() => setIsMuted(!isMuted)}
+                    />
+                  </div>
+                )}
+                <div className="infoIconContainer">
+                  <BsFullscreenExit
+                    size="5em"
+                    onClick={fullscreenHandle.exit}
+                  />
+                </div>
+              </div>
+            ) : null}
+          </div>
         </FullScreen>
 
         <div
@@ -112,7 +165,7 @@ export const Startpage = () => {
               <VscDebugRestart size={"2em"} />
               <b>Neustart</b>
             </div>
-            <div className="infoIconContainer" onClick={handle.enter}>
+            <div className="infoIconContainer" onClick={fullscreenHandle.enter}>
               <BsArrowsFullscreen size={"2em"} />
               <b>Vollbild</b>
             </div>
