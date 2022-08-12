c135b87e3cb8171e8598fd1b0c33d6cbc4a85583
ScarfaceHBC <p.jipp87@gamil.com>
2022-08-12 09:24:33 +0200

    ....

--- src/_data/MovieDB.js
+++ src/_data/MovieDB.js
@@ -4,7 +4,7 @@ export const MovieDB = [
     id: 1,
     cover: "/src/Assets/images/Covers/Captain_America_The_First_Avenger.jpg",
     trailer:
-      "http://videos.hd-trailers.net/Captain_America_The_First_Avenger_Trailer_C_5.1-1080p-HDTN.mp4",
+      "http://movietrailers.apple.com/movies/paramount/captainamerica/captainamerica-tlr1_h1080p.mov",
     description:
       "1942: Die USA ziehen in den Kampf gegen Hitler-Deutschland. Steve Rogers meldet sich freiwillig zur Armee, wird aber aufgrund seines schwächlichen Gesundheitszustands als ungeeignet eingestuft. Mit Hilfe des `Project: Rebirth' soll er trotzdem in den Kampf gegen die Nazis ziehen. Der kränkliche Steve wird in einen Super-Soldaten namens Captain America transformiert. Gleichzeitig versucht sich bei den Nazis auch Johann Schmidt daran, mit einer Geheimwaffe den Rest der Welt zu unterjochen.",
   },
@@ -53,5 +53,156 @@ export const MovieDB = [
       "http://trailers.apple.com/movies/paramount/thor/thor-tlr1_h1080p.mov",
     description:
       "Thor ist ein nordischer Donnergott, der von seinem Göttervater Odin aufgrund einer hinterhältigen Intrige vom fernen Asgard auf die Erde verbannt wird. Dort lernt der mächtige, aber arrogante Hüne, was es wirklich braucht, um einer der größten Superhelden zu sein. Mit Hilfe seines gigantischen Hammers Mjölnir wird Thor zu einem der größten Beschützer der Menschheit. Die Astrophysikerin Jane Foster hilft ihm bei seinen Versuchen, wieder nach Asgard zurückzukehren.",
+  },
+  {
+    name: "The Avengers",
+    id: 7,
+    cover: "/src/Assets/images/Covers/The_Avengers.jpg",
+    trailer:
+      "http://trailers.apple.com/movies/marvel/theavengers/avengers-tlr1_h1080p.mov",
+    description:
+      "Loki, der Halbbruder des Donnergottes Thor, will die Erde unterjochen. Um der Menschheit diese Zukunft zu ersparen, ruft Nick Fury, Chef der geheimen Regierungsbehörde und Friedensorganisation S.H.I.E.L.D., die größten Helden der Welt zusammen. Captain America wird aus seinem jahrzehntelangen Schlaf geweckt und schließlich greift Fury zudem auf die Dienste des egozentrischen Tony Stark, aka Iron Man, zurück. Auch der Wissenschaftler Dr. Bruce Banner wird ins Team gelockt.",
+  },
+  {
+    name: "Iron Man 3",
+    id: 8,
+    cover: "/src/Assets/images/Covers/Iron_Man_3.jpg",
+    trailer:
+      "http://trailers.apple.com/movies/marvel/ironman3/ironman3-tlr1-m4mb0_h1080p.mov",
+    description:
+      'Nach den traumatischen Ereignissen in New York, muss Iron Man sich gegen den nächsten Feind wappnen. "Der Mandarin" taucht in der Stadt auf und droht den USA mit Terroranschlägen. Doch auch das private Umfeld von Tony Stark ist gefährdet, als sein Sicherheitsmann Happy angegriffen und schwer verletzt wird. Stark fordert ihn zu einem Duell heraus, doch sein Gegner ist mächtig. Eine weitere Bedrohung zeigt sich in Form von zwei Gestalten aus der Vergangenheit Starks, die ihn nicht kalt lassen.',
+  },
+  {
+    name: "Thor - The Dark Kindom",
+    id: 9,
+    cover: "/src/Assets/images/Covers/Thor_The_Dark_Kingdom.jpg",
+    trailer: "http://videos.hd-trailers.net/Thor2_TLR-1-1080p-HDTN.mp4",
+    description:
+      "Astrophysikerin Jane Foster entdeckt eine gefährliche, mysteriöse Substanz. Als diese von ihr Besitz ergreift, macht sich Thor auf, um sie zu retten. Aber auch der diabolische Malekith ist hinter der Substanz her, um mit ihren Kräften das Universum zu unterwerfen. Thor muss sich mit seinem sinistren Adoptivbruder Loki verbünden, um den Bösewichtern zu zeigen, wo der Hammer hängt. Bald aber ist er nicht mehr sicher, auf wessen Seite Loki steht.",
+  },
+  {
+    name: "The Return of the First Avenger",
+    id: 10,
+    cover: "/src/Assets/images/Covers/Return_of_tehe_First_Avenger.jpg",
+    trailer:
+      "https://movietrailers.apple.com/movies/marvel/captainamericathewintersoldier/captainamerica2-tlr2_a720p.m4v#t=54.273306",
+    description:
+      "70 Jahre lag Steve Rogers alias Captain America im Kälteschlaf. Nur langsam findet sich der Superheld in der Gegenwart zurecht. Da wartet schon die nächste Herausforderung: Rogers stößt auf verdächtige Machenschaften innerhalb von S.H.I.E.L.D., bricht mit der Organisation und wird von seinen ehemaligen Mitstreitern als Verräter gejagt. Gemeinsam mit Falcon und Black Widow deckt er eine gewaltige Verschwörung auf.",
+  },
+  {
+    name: "Guardiens of the Galaxy",
+    id: 11,
+    cover: "/src/Assets/images/Covers/Guardiens_of_the_Galaxy.jpg",
+    trailer: "http://videos.hd-trailers.net/gotg-tlr1-1080p.mp4",
+    description:
+      "Peter Quill ist nicht der umgänglichste Vertreter der Spezies Mensch. Doch er hat dafür Gründe: Als Kind wurde er von Aliens unter der Führung des Weltraumpiraten Yondu Udonta entführt. Als Erinnerung bleibt ihm nur eine Kassette mit den Lieblingssongs seiner Mutter. Als Star Lord reist er nun durch das Weltall und trifft auf andere Außenseiter. Sie gründen unter seiner Führung die `Guardians of the Galaxy' und wollen gemeinsam den Weltraum schützen, der in großer Gefahr ist.",
+  },
+  {
+    name: "Guardiens of the Galaxy Vol.2",
+    id: 12,
+    cover: "/src/Assets/images/Covers/Guardiens_of_the_Galaxy_vol_2.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/guardians-of-the-galaxy-vol-2/guardians-of-the-galaxy-vol-2-trailer-2_h1080p.mov",
+    description:
+      "Nachdem `Star-Lord' Peter Quill erfahren hat, dass er nur zur Hälfte Mensch ist, will er das Rätsel seiner Herkunft lösen. Auf der Suche nach Antworten macht er gemeinsam mit seinen Freunden, der undurchsichtigen Gamora, dem Muskelpaket Drax, dem genetisch modifizierten Waschbären Rocket und Baby Groot, das Universum unsicher. Auf ihren Abenteuern bekommt die Truppe es nicht nur mit Quills Ersatzvater Yondu und Gamoras Schwester Nebula zu tun, sondern auch mit einigen neuen Freunden und Feinden.",
+  },
+  {
+    name: "Avengers: Age of Ultron",
+    id: 13,
+    cover: "/src/Assets/images/Covers/The_Avengers_Age_of_Ultron.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/avengersageofultron/avengersageofultron-tlr1_h1080p.mov",
+    description:
+      "Die Avengers befinden sich im Kampf um Lokis Zepter. Es gelingt ihnen, es zurückzubekommen, doch dann entdecken sie in dessen Inneren eine Künstliche Intelligenz: Ultron. Tony Stark ist sogleich Feuer und Flamme und möchte sie im Kampf für den Frieden einsetzen, zur Abschirmung von außerirdischen Gefahren. Ultron hat jedoch anderes im Sinn, nämlich die Ausrottung der Menschheit. Und so müssen die Avengers bald alles daran setzen, Ultron in seine Schranken zu verweisen.",
+  },
+  {
+    name: "Ant-Man",
+    id: 14,
+    cover: "/src/Assets/images/Covers/Ant_Man.jpeg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/antman/antman-tlr2_h1080p.mov",
+    description:
+      "Der Biochemiker Dr. Hank Pym hat einen Anzug entwickelt, mit dessen Hilfe sich der Träger auf Ameisengröße schrumpfen kann. Der Meisterdieb Scott Lang stiehlt den Anzug, wird dabei aber von Hank beobachtet. Die beiden schließen sich zusammen, um die Fähigkeiten des jeweils anderen zu nutzen. Gemeinsam planen sie einen Raub, bei eine alte Technologie des Doktors zurück in die Hände seines Besitzers wandern soll.",
+  },
+  {
+    name: "The First Avenger: Civil War",
+    id: 15,
+    cover: "/src/Assets/images/Covers/The_First_Avenger_Civil_War.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/captainamericacivilwar/captainamericacivilwar-tlr1_h1080p.mov",
+    description:
+      "Nachdem ein Einsatz einer Superhelden-Nachwuchstruppe eine Katastrophe ausgelöst hat, bei der Schulkinder ums Leben gekommen sind, führt die Regierung den Superhero Registration Act ein. Demzufolge müssen sich Superhelden registrieren und ihre Kräfte in den Dienst des Staates stellen. Teil dessen ist auch die Preisgabe ihrer Identität. Während Tony Stark keinen Hehl aus seinem Alter Ego Iron Man macht, sieht Captain America dies anders. Es kommt zum Bruch der Avengers und zum Bürgerkrieg.",
+  },
+  {
+    name: "Black Panther",
+    id: 16,
+    cover: "/src/Assets/images/Covers/Black_Panther.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/black-panther/black-panther-trailer-1_h1080p.mov",
+    description:
+      "T'Challa begibt sich zurück in seine afrikanische Heimat Wakanda, wo er sich darauf vorbereitet, der rechtmäßige König zu werden. Doch der Söldner Erik Killmonger hat vor, ihm seine Regentschaft streitig zu machen und sucht nach Komplizen. Also braucht T'Challa, der auch Black Panther genannt wird, Unterstützung. Die findet er beim CIA-Agenten Everett sowie einer exzellent ausgebildeten Gruppe von Kriegerinnen.",
+  },
+  {
+    name: "Spider-Man: Homecoming",
+    id: 17,
+    cover: "/src/Assets/images/Covers/Spiderman_Homecoming.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/sony_pictures/spiderman-homecoming/spiderman-homecoming-trailer-1_h1080p.mov",
+    description:
+      'Der Teenager Peter Parker alias "Spider-Man" kehrt zurück nach New York, um bei seiner Tante zu wohnen. Sein Mentor Tony Stark nimmt ihn unter seine Fittiche, um ihm dabei zu helfen, mit seiner neuen Identität klarzukommen. Peter muss seinem geregelten Alltag nachgehen, lässt sich aber allzu leicht an den Gedanken an seine Fähigkeiten und deren Nutzen ablenken. Als der Vogelman Vulture für Unruhe in der Stadt sorgt, sieht Peter seine Chance gekommen.',
+  },
+  {
+    name: "Doctor Strange",
+    id: 18,
+    cover: "/src/Assets/images/Covers/Doctor_Strange.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/marvelsdoctorstrange/doctor-strange-tlr2_h1080p.mov",
+    description:
+      'Der brillante und egozentrische Neurochirurg Doctor Stephen Strange kann nach einem schlimmen Autounfall nicht mehr operieren. Auf der Suche nach Heilung reist er nach Tibet, wo er eine geheimnisvolle Frau kennenlernt, die nur als "The Ancient One" bekannt ist. Sie führt ihn in die Geheimnisse der Magie und verborgener Dimensionen ein und bildet ihn zum "Obersten Zauberer" aus. Doch schon bald muss Doctor Strange die Welt vor einer Bedrohung aus einer anderen Dimension beschützen.',
+  },
+  {
+    name: "Thor: Tag der Entscheidung",
+    id: 19,
+    cover: "/src/Assets/images/Covers/Thor_Ragnarok.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/thor-ragnarok/thor-ragnarok-trailer-1_h1080p.mov",
+    description:
+      "Weit abseits seines Heimatorts Asgard gerät Donnergott Thor in die Fänge des Feuerdämons Surtur, aus dessen er sich ohne seinen Hammer Mjölnir nur schwer lösen kann. Für Thor beginnt ein Kampf um die Zeit, denn Hela ist nach tausenden von Jahren aus dem Gefängnis freigekommen und droht eine Götterdämmerung einzuleiten, die Thors Heimat Asgard auslöschen würde. Auf dem Weg nach Asgard muss Thor zunächst Gladiatoren bekämpfen und den stärksten Kämpfer Sakaars besiegen.",
+  },
+  {
+    name: "Ant-Man and the Wasp",
+    id: 20,
+    cover: "/src/Assets/images/Covers/Ant_Man_and_the_Wasp.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/ant-man-and-the-wasp/ant-man-and-the-wasp-trailer-1-usca_h1080p.mov",
+    description:
+      "Scott Lang, auch bekannt als Ant-Man, findet sich zwischen zwei Rollen gefangen: der als Superheld und der als Vater von Cassie. Hope van Dyne und Dr. Hank Pym stürzen ihn jedoch in ein neues Abenteuer. Lang muss wieder den Superhelden-Anzug anziehen und sich mit Wasp zusammenschließen. Die dringende Suche nach Hopes Mutter führt die beiden zu geheimen Enthüllungen aus der Vergangenheit. Sie bekommen es außerdem mit einem neuen, mächtigen Gegner zu tun.",
+  },
+  {
+    name: "Avengers: Infinity War",
+    id: 21,
+    cover: "/src/Assets/images/Covers/Avengers_Infinity_War.jpg",
+    trailer:
+      "http://movietrailers.apple.com/movies/marvel/avengers-infinity-war/avengers-infinity-war-trailer-1_h1080p.mov",
+    description:
+      "Der intergalaktische Titan Thanos verfolgt den Plan, alle sechs Infinity-Steine zu versammeln und mit ihrer Macht das halbe Universum auszulöschen. Die Avengers, die seit der Auseinandersetzung zwischen Iron Man und Captain America hoffnungslos gespalten sind, müssen sich zusammenraufen, um sich dieser Bedrohung stellen zu können. Unterstützung erhalten sie dabei von Dr. Strange, den Guardians of the Galaxy um Star-Lord Peter Quill sowie von Black Panther, dem neuen König von Wakanda.",
+  },
+  {
+    name: "Avengers: Endgame",
+    id: 22,
+    cover: "/src/Assets/images/Covers/Avengers_Endgame.jpg",
+    trailer:
+      "https://movietrailers.apple.com/movies/marvel/avengers-endgame/avengers-endgame-trailer-1_h1080p.mov",
+    description:
+      "Nach den vorangegangenen Ereignissen liegt das Universum dank des Titanen Thanos in Schutt und Asche. Die übrig gebliebenen Avengers - Thor, Black Widow, Captain America and Bruce Banner - müssen versuchen, ihre besiegten Verbündeten ein letztes mal zu versammeln, um in einem finalen Kampf gegen Thanos zu bestehen und das Chaos im Universum umzukehren.",
+  },
+  {
+    name: "Spider-Man: Far From home",
+    id: 23,
+    cover: "/src/Assets/images/Covers/Spiderman_Far_From_Home.jpg",
+    trailer:
+      "https://movietrailers.apple.com/movies/sony_pictures/spider-man-far-from-home/spider-man-far-from-home-trailer-1_h1080p.mov",
+    description:
+      "Der freundliche Junge Peter Parker ist der Superheld, der als Spiderman bekannt ist. Er beschließt, gemeinsam mit seinen Freunden Ned, MJ, Flash und anderen auf eine Reise zu gehen. Natürlich ist auch im Urlaub sein Einsatz gefragt.",
   },
 ];
